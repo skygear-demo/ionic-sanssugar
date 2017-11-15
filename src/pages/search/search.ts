d1abc20d0cec68a74f3eb6affd9e68383375a1c3
david90 <david.ng@oursky.com>
2017-11-15 17:23:53 +0800

    Search for and add items

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, ModalController, NavController, NavParams } from 'ionic-angular';
 
 import { Item } from '../../models/item';
 import { Items } from '../../providers/providers';
@@ -13,13 +13,19 @@ export class SearchPage {
 
   currentItems: any = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public items: Items) { }
+  constructor(public navCtrl: NavController, 
+    public navParams: NavParams,
+    public modalCtrl: ModalController,
+    public items: Items,) {
+    this.currentItems = this.items.query();
+  }
 
   /**
    * Perform a service for the proper items.
    */
-  getItems(ev) {
 
+
+  getItems(ev) {
     let val = ev.target.value;
     if (!val || !val.trim()) {
       this.currentItems = this.items.query(); // show all by default
@@ -37,6 +43,16 @@ export class SearchPage {
     this.navCtrl.push('ItemDetailPage', {
       item: item
     });
+  }
+
+  addItem() {
+    let addModal = this.modalCtrl.create('ItemCreatePage');
+    addModal.onDidDismiss(item => {
+      if (item) {
+        this.items.add(item);
+      }
+    })
+    addModal.present();
   }
 
 }
